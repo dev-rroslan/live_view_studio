3ad7472fb7592c3330425190d22e9b716a73074c
dev-rroslan <user.email>
2023-10-07 13:21:03 +0800

    edit runtime

--- config/runtime.exs
+++ config/runtime.exs
@@ -53,6 +53,10 @@ if config_env() == :prod do
 
   config :live_view_studio, LiveViewStudioWeb.Endpoint,
     url: [host: host, port: 443, scheme: "https"],
+    check_origin: [
+      "https://undi.online",
+      "https://undi.fly.dev",
+    ],
     http: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
